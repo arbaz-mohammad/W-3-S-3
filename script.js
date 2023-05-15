5a137911b6ef3fe71d0ed5abbd150a5f26bddab3
arbaz-mohammad <127589977+arbaz-mohammad@users.noreply.github.com>
2023-05-15 22:52:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,298 @@
+function getSeatingArrangement(seat, row)
+ {
+    if (row < 1 || seat < 1) {
+      return "Invalid input. Row and seat numbers must be positive.";
+    }
+  
+    if (row ==1 ) {
+        if(seat==1){
+            return "lower berth"
+        }
+        else if (seat==2){
+            return "middle berth"
+        }
+        else if (seat==3){
+            return "upper berth"
+        }
+        else(seat==7)
+        {
+            return "side lower berth"
+        }
+        
+      
+  }
+
+else if (row ==2 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==3 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==4 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==5 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==6 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==7 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==8 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==9 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+else if (row ==10 ) {
+    if(seat==4){
+        return "lowerberth"
+    }
+    else if (seat==5){
+        return "middle berth"
+    }
+    else if (seat==6){
+        return "upper berth"
+    }
+    else if (seat==9){
+        return "side lower berth"
+    }
+
+    else if (seat==10){
+        return "uperberth"
+    }
+    else if (seat==11){
+        return "middle berth"
+    }
+    else if (seat==8){
+        return "lower berth"
+    }
+    else(seat==15)
+    {
+        return "side lower berth"
+    }
+    
+  
+}
+
+
+}
+  console.log(getSeatingArrangement(15,2))
